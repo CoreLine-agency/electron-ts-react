febb705027fcc20c81521d4908f5d64ff6d5d0f8
Ivo Katunaric <ivo@coreline.agency>
2018-11-14 20:41:37 +0100

    init

---
+++ src/renderer/App.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import fs from 'fs';
+
+import './App.css';
+const logo = require('./logo.svg');
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
+
+console.log('fs from react!', fs);
